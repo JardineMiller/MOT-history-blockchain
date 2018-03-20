3b42cd2a5c444637c6f6f7c58ef819535faa7aa6
Jardine <jardine.miller@gmail.com>
2018-03-20 20:43:11 +0000

    made a few more examples

--- Crypto-C-Sharp/Program.cs
+++ Crypto-C-Sharp/Program.cs
@@ -8,9 +8,9 @@ namespace CryptoCSharp
         {
             var blockchain = new Blockchain();
 
-            blockchain.CreateTransaction(new Transaction("address1", "address2", 100));
-            blockchain.CreateTransaction(new Transaction("address2", "address1", 50));
-            blockchain.CreateTransaction(new Transaction("address2", "address1", 500));
+            blockchain.CreateTransaction(new Transaction(null, "address1", 100));
+            blockchain.CreateTransaction(new Transaction("address1", "address2", 50));
+            blockchain.CreateTransaction(new Transaction("address2", "address1", 10));
 
 
             Console.WriteLine("Starting the miner...");
@@ -23,6 +23,15 @@ namespace CryptoCSharp
             blockchain.MinePendingTransactions("Jardine-Address");
 
             Console.WriteLine("Balance of Jardine-Address: " + blockchain.GetAddressBalance("Jardine-Address"));
+            Console.WriteLine("Balance of address1: " + blockchain.GetAddressBalance("address1"));
+            Console.WriteLine("Balance of address2: " + blockchain.GetAddressBalance("address2"));
+
+            blockchain.CreateTransaction(new Transaction("Jardine-Address", "address2", 50));
+            blockchain.MinePendingTransactions("Jardine-Address");
+
+            Console.WriteLine("Balance of Jardine-Address: " + blockchain.GetAddressBalance("Jardine-Address"));
+            Console.WriteLine("Balance of address1: " + blockchain.GetAddressBalance("address1"));
+            Console.WriteLine("Balance of address2: " + blockchain.GetAddressBalance("address2"));
         }
     }
 }
